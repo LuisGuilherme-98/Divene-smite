10b6d575d894c4d82b21353d8a0b1f8c64285e88
LUIS GUILHERME MANHANI SILVA <luisguilherme123456.lgms@gmail.com>
2024-09-08 11:14:45 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa")
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Nada foi encontrado</p>"
+        return
+    }
+    campoPesquisa = campoPesquisa.toLowerCase()
+    
+    let resultados = "";
+    let titulo = "";
+    let descricao ="";
+//para cada dado dentro da lista de dados
+for (let plano of planos) {
+    titulo = plano.titulo.toLowerCase()
+    descricao = plano.descricao.toLowerCase()
+    if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+        resultados +=`
+ <div class="item-resultado">
+        <h2>
+          <a href="#" target="_blank">${plano.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${plano.descricao}</p>
+    </div>
+    `;
+    }
+    if(!resultados){
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+}
+section.innerHTML = resultados
+}
+//console.log(dados);

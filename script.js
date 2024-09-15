693e019af29a5b5e4c8fa3640bd7b6a913d3f0d8
LUIS GUILHERME MANHANI SILVA <luisguilherme123456.lgms@gmail.com>
2024-09-14 21:10:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-let planos = [
-{
-    titulo : "Planos",
-    descricao: "Em Dungeons & Dragons, o universo não se limita a um único mundo. Na verdade, existe uma infinidade de planos, cada um com suas próprias características, habitantes e regras. Esses planos formam um vasto multiverso, onde deuses, demônios, fadas e outras criaturas coexistem. Cada uma com suas próprias leis da física, magia e moralidade. Eles podem ser tão diferentes quanto um reino de fogo eterno e um jardim encantado."
-},
-{
-    titulo: "Plano Material",
-    descricao: "O plano onde a maioria das aventuras se passa, um mundo diversificado com reinos, cidades, florestas e oceanos."
+function rolarD20() {
+  const resultado = Math.floor(Math.random() * 20) + 1;
+  alert(`Você rolou um D20 e tirou: ${resultado}`);
 }
-
-];
